b34f37d9221a017d4f057df768a49992e576eddc
Takuya Matsuyama <nora@odoruinu.net>
2018-02-28 11:00:14 +0900

    fix: Fix bug where it won't work at all

--- src/paste-text.js
+++ src/paste-text.js
@@ -2,7 +2,7 @@ import { clipboard } from 'electron'
 
 export function pasteText () {
   const text = clipboard.readText()
-  if (typeof text === 'text') {
+  if (typeof text === 'string') {
     const cm = inkdrop.getActiveEditor().codeMirror
     cm.replaceSelection(text)
   }
